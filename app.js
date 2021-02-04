23daabcc66c2f620ae8ee99dbd6d682e3031ff29
Andrew Irving <73566194+irvingam@users.noreply.github.com>
2021-02-04 12:54:38 -0500

    added todo close button and img click functions
    organized selectors, functions, and events within the javascript

--- app.js
+++ app.js
@@ -1,28 +1,54 @@
-// Selectors
+                        /////// Selectors ///////
+
+// Nav Bar Selectors
 const navToggle = document.querySelector('.nav-toggle')
 const navLinks = document.querySelectorAll('.nav__link')
 
+// Calculator App Modal Selectors
 const calcModal = document.getElementById('calc--modal')
 const calcImg = document.getElementById('calc--img')
-const calcModalImg = document.querySelector('.modal-img')
-const closeBtnModal = document.getElementsByClassName('close')[0]
-//Events
+const calcModalImg = document.getElementById('calc-modal-img')
+
+// ToDo App Modal Selectors
+const todoModal = document.getElementById('todo--modal')
+const todoImg = document.getElementById('todo--img')
+const todoModalImg = document.getElementById('todo-modal-img')
+
+// Modal Global Selectors
+const calcCloseBtn = document.getElementsByClassName('close')[0]
+const todoCloseBtn = document.getElementsByClassName('close')[1]
+                        /////// Events  ///////
+
+// Open Nav Hamburger Event
 navToggle.addEventListener('click', () => {
     document.body.classList.toggle('nav-open')
 })
 
                         /////// Functions ///////
+
+// Nav Hamburger Links
 navLinks.forEach(link => {
     link.addEventListener('click', () => {
         document.body.classList.remove('nav-open')
     })
 })
-
+/// Calculator Portforlio Item Modal
 calcImg.onclick = function() {
     calcModal.style.display = 'block'
     calcModalImg.src = this.src
 }
 
-closeBtnModal.onclick = function() {
-    calcModal.style.display =  'none'
+calcCloseBtn.onclick = function() {
+    calcModal.style.display = 'none'
+   
+}
+
+// ToDo Portforlio Item Modal
+todoImg.onclick = function() {
+    todoModal.style.display = 'block'
+    todoModalImg.src = this.src
+}
+
+todoCloseBtn.onclick = function() {
+    todoModal.style.display = 'none'
 }
