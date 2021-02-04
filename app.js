0525405ab4fef30324287ba8da096345c9ec313b
Andrew Irving <73566194+irvingam@users.noreply.github.com>
2021-02-03 21:53:52 -0500

    added html, css, and javascript for modal in calc project

--- app.js
+++ app.js
@@ -2,15 +2,27 @@
 const navToggle = document.querySelector('.nav-toggle')
 const navLinks = document.querySelectorAll('.nav__link')
 
+const calcModal = document.getElementById('calc--modal')
+const calcImg = document.getElementById('calc--img')
+const calcModalImg = document.querySelector('.modal-img')
+const closeBtnModal = document.getElementsByClassName('close')[0]
 //Events
 navToggle.addEventListener('click', () => {
     document.body.classList.toggle('nav-open')
 })
 
+                        /////// Functions ///////
 navLinks.forEach(link => {
     link.addEventListener('click', () => {
         document.body.classList.remove('nav-open')
     })
 })
 
-//Functions
+calcImg.onclick = function() {
+    calcModal.style.display = 'block'
+    calcModalImg.src = this.src
+}
+
+closeBtnModal.onclick = function() {
+    calcModal.style.display =  'none'
+}
