fa612801b0e90b2c321c9a95bc73240ae10cc654
Andrew Irving <73566194+irvingam@users.noreply.github.com>
2021-02-04 22:12:42 -0500

    added modal methods
    allows the activation of modals for employee log app, budget app, and todo app. also adds functionality for the modal close button.

--- app.js
+++ app.js
@@ -14,9 +14,21 @@ const todoModal = document.getElementById('todo--modal')
 const todoImg = document.getElementById('todo--img')
 const todoModalImg = document.getElementById('todo-modal-img')
 
+// Budget App Modal Selectors
+const budgetModal = document.getElementById('budget--modal')
+const budgetImg = document.getElementById('budget--img')
+const budgetModalImg = document.getElementById('budget-modal-img')
+
+// Employee Log App Modal Selectors
+const logModal = document.getElementById('log--modal')
+const logImg = document.getElementById('log--img')
+const logModalImg = document.getElementById('log-modal-img')
+
 // Modal Global Selectors
 const calcCloseBtn = document.getElementsByClassName('close')[0]
 const todoCloseBtn = document.getElementsByClassName('close')[1]
+const budgetCloseBtn = document.getElementsByClassName('close')[2]
+const logCloseBtn = document.getElementsByClassName('close')[3]
                         /////// Events  ///////
 
 // Open Nav Hamburger Event
@@ -32,7 +44,7 @@ navLinks.forEach(link => {
         document.body.classList.remove('nav-open')
     })
 })
-/// Calculator Portforlio Item Modal
+/// Calculator Portfolio Item Modal
 calcImg.onclick = function() {
     calcModal.style.display = 'block'
     calcModalImg.src = this.src
@@ -43,7 +55,7 @@ calcCloseBtn.onclick = function() {
    
 }
 
-// ToDo Portforlio Item Modal
+// ToDo Portfolio Item Modal
 todoImg.onclick = function() {
     todoModal.style.display = 'block'
     todoModalImg.src = this.src
@@ -51,4 +63,24 @@ todoImg.onclick = function() {
 
 todoCloseBtn.onclick = function() {
     todoModal.style.display = 'none'
+}
+
+// Budget Portfolio Item Modal 
+budgetImg.onclick = function() {
+    budgetModal.style.display = 'block'
+    budgetModalImg.src = this.src
+}
+
+budgetCloseBtn.onclick = function() {
+    budgetModal.style.display = 'none'
+}
+
+// Employee Portfolio Item Modal
+logImg.onclick = function() {
+    logModal.style.display = 'block'
+    logModalImg.src = this.src
+}
+
+logCloseBtn.onclick = function() {
+    logModal.style.display = 'none'
 }
